7dc5ee41ea8b6fe8c393975aba527183a2b36f8a
Jeong mokjin <jungmokjin@gmail.com>
2026-02-24 15:48:28 +0900

    로컬 브랜치 수정사항 적용

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        로컬 브랜치에서 수정한 수정사항
       </p>
     </>
   )
